cfc7d18062da8324be2bcc824ff2869159c3ec8c
Peter Corke <peter.i.corke@gmail.com>
2020-05-03 15:33:48 +1000

    remove cruft, fix inv

--- spatialmath/pose3d.py
+++ spatialmath/pose3d.py
@@ -14,118 +14,9 @@ from spatialmath.base import argcheck
 import spatialmath.base as tr
 from spatialmath import super_pose as sp
 
-class SO2(sp.SuperPose):
-    
-    # SO2()  identity matrix
-    # SO2(angle, unit)
-    # SO2( obj )   # deep copy
-    # SO2( np )  # make numpy object
-    # SO2( nplist )  # make from list of numpy objects
-    
-    # constructor needs to take ndarray -> SO2, or list of ndarray -> SO2
-    def __init__(self, arg = None, *, unit='rad'):
-        super().__init__()  # activate the UserList semantics
-        
-        if arg is None:
-            # empty constructor
-            self.data = [np.eye(2)]
-        
-        elif argcheck.isvector(arg):
-            # SO2(value)
-            # SO2(list of values)
-            self.data = [tr.rot2(x, unit) for x in argcheck.getvector(arg)]
-            
-        else:
-            super().arghandler(arg)
 
-    @classmethod
-    def rand(cls, *, range=[0, 2*math.pi], unit='rad', N=1):
-        rand = np.random.uniform(low=range[0], high=range[1], size=N)  # random values in the range
-        return cls([tr.rot2(x) for x in argcheck.getunit(rand, unit)])
     
-    @classmethod
-    def isvalid(self, x):
-        return tr.isrot2(x, check=True)
-
-    @property
-    def T(self):
-        return SO2(self.A.T)
-    
-    def inv(self):
-        return SO2(self.A.T)
-    
-    # for symmetry with other 
-    @classmethod
-    def R(cls, theta, unit='rad'):
-        return SO2([tr.rot1(x, unit) for x in argcheck.getvector(theta)])
-    
-    @property
-    def angle(self):
-        """Returns angle of SO2 object matrices in unit radians"""
-        angles = []
-        for each_matrix in self:
-            angles.append(math.atan2(each_matrix[1, 0], each_matrix[0, 0]))
-        # TODO !! Return list be default ?
-        if len(angles) == 1:
-            return angles[0]
-        elif len(angles) > 1:
-            return angles
-
-class SE2(SO2):
-    # constructor needs to take ndarray -> SO2, or list of ndarray -> SO2
-    def __init__(self, x = None, y = None, theta = None, *, unit='rad'):
-        super().__init__()  # activate the UserList semantics
-        
-        if x is None:
-            # empty constructor
-            self.data = [np.eye(3)]
-        
-        elif all(map(lambda x: isinstance(x, (int,float)), [x, y, theta])):
-            # SE2(x, y, theta)
-            self.data = [tr.trot2(theta, t=[x,y], unit=unit)]
-            
-        elif argcheck.isvector(x) and argcheck.isvector(y) and argcheck.isvector(theta):
-            # SE2(xvec, yvec, tvec)
-            xvec = argcheck.getvector(x)
-            yvec = argcheck.getvector(y, dim=len(xvec))
-            tvec = argcheck.getvector(theta, dim=len(xvec))
-            self.data = [tr.trot2(_t, t=[_x, _y]) for (_x, _y, _t) in zip(xvec, yvec, argcheck.getunit(tvec, unit))]
-            
-        elif isinstance(x, np.ndarray) and y is None and theta is None:
-            assert x.shape[1] == 3, 'array argument must be Nx3'
-            self.data = [tr.trot2(_t, t=[_x, _y], unit=unit) for (_x, _y, _t) in x]
-            
-        else:
-            super().arghandler(x)
-
-    @property
-    def T(self):
-        raise NotImplemented('transpose is not meaningful for SE3 object')
-
-    @classmethod
-    def rand(cls, *, xrange=[-1, 1], yrange=[-1, 1], trange=[0, 2*math.pi], unit='rad', N=1):
-        x = np.random.uniform(low=xrange[0], high=xrange[1], size=N)  # random values in the range
-        y = np.random.uniform(low=yrange[0], high=yrange[1], size=N)  # random values in the range
-        theta = np.random.uniform(low=trange[0], high=trange[1], size=N)  # random values in the range
-        return cls([tr.trot2(t, t=[x,y]) for (t,x,y) in zip(x, y, argcheck.getunit(theta, unit))])
-    
-    @classmethod
-    def isvalid(self, x):
-        return tr.ishom2(x, check=True)
-
-    @property
-    def t(self):
-        return self.A[:2,2]
-    
-    @property
-    def R(self):
-        return SO2(self.A[:2,:2])
-    
-    def inv(self):
-        return SO2(self.A.T)
-    ArithmeticError()
-    
-class SO3(sp.SuperPose):
+class SO3(sp.SMPose):
     
     # SO2()  identity matrix
     # SO2(angle, unit)
@@ -213,7 +104,7 @@ class SO3(sp.SuperPose):
     def angvec(cls, theta, v, *, unit='rad'):
         return cls(tr.angvec2r(theta, v, unit=unit), check=False)
 
-class SE3(sp.SuperPose):
+class SE3(sp.SMPose):
 
     def __init__(self, arg = None, *, unit='rad', check=True):
         super().__init__()  # activate the UserList semantics
@@ -267,9 +158,9 @@ class SE3(sp.SuperPose):
     
     def inv(self):
         if len(self) == 1:
-            return SO3(self.A.T)
+            return SE3(tr.rt2tr(self.R.T, -self.R.T @ self.t))
         else:
-            return SO3([x.T for x in self.A])
+            return SE3([SE3(tr.rt2tr(x.R.T, -x.R.T @ x.t)) for x in self])
     
     @property
     def n(self):
@@ -286,6 +177,10 @@ class SE3(sp.SuperPose):
     @property
     def t(self):
         return self.A[:3,3]
+    
+    @property
+    def R(self):
+        return self.A[:3,:3]
     
     
 
@@ -316,79 +211,7 @@ class SE3(sp.SuperPose):
 
 if __name__ == '__main__':
     
-    R = SO3()
-
-    # print(isinstance(R, SO3))
-    # print(isinstance(R, sp.SuperPose))
-
-    # a = SO2(0.2)
-    # b = SO2(a)
-    # print(a+a)
-    # print(a*a)
-
-    # b = SO2(0.1)
-    # b.append(a)
-    # b.append(a)
-    # b.append(a)
-    # b.append(a)
-    # print(len(a))
-    # print(len(b))
-    # print(b)
-
-    # c = SO2(0.3)
-    # c.extend(a)
-    # c.extend(b)
-    # print(len(c))
-
-    # d = SO2(0.4)
-    # d.append(b)
-    # print(len(d))
-    # print(d)
-
-
-    # if __name__ == '__main__':
-
-    #     import numpy.testing as nt
-            
-    #     class Test_check(unittest.TestCase):
-            
-    #         def test_unit(self):
-                
-
-    #print(a)
-    #print(a*a)
-    #c = SO2(0)
-    #
-    #b = a
-    #print(len(b))
-    #b.append(c)
-    #b.append(c)
-    #print(len(b))
-    #print(b)
-    #print(b)
-    #print(b[0])
-    #print(type(a))
-    #print(type(b))
-
-    #arr = [SO2(0), SO2(0.1), SO2(0.2)]
-    #print(arr)
-    #b = np.array(arr)
-    #print(b)
-    #print('--')
-    #print(arr[0])
-    #print(b[1])
-    #print(b*a)
-    
-
-    # import pathlib
-    # import os.path
-    
-    # runfile(os.path.join(pathlib.Path(__file__).parent.absolute(), "test_tr.py") )
- 
-    
     import pathlib
     import os.path
     
-    runfile(os.path.join(pathlib.Path(__file__).parent.absolute(), "test_pose.py") )
-    
-    T = SE3()
+    runfile(os.path.join(pathlib.Path(__file__).parent.absolute(), "test_pose3d.py") )
