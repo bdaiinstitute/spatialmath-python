fa244f09c519460048e938c1814bd19a76c899d1
Peter Corke <peter.i.corke@gmail.com>
2020-05-03 15:32:35 +1000

    fix indentation, works

--- spatialmath/test_quaternion.py
+++ spatialmath/test_quaternion.py
@@ -1,18 +1,22 @@
-    import numpy.testing as nt
-    import unittest
+import numpy.testing as nt
+import unittest
     
-    from transforms import *
+from spatialmath.quaternion import *
             
-    class TestUnitQuaternion(unittest.TestCase):
+class TestUnitQuaternion(unittest.TestCase):
         
-        def test_constructor(self):
-            nt.assert_array_almost_equal(UnitQuaternion().vec, np.r_[1,0,0,0])
+    def test_constructor(self):
+        nt.assert_array_almost_equal(UnitQuaternion().vec, np.r_[1,0,0,0])
+        
+        nt.assert_array_almost_equal(UnitQuaternion.Rx(90,'deg').vec, np.r_[1,1,0,0]/math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rx(-90,'deg').vec, np.r_[1,-1,0,0]/math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Ry(90,'deg').vec, np.r_[1,0,1,0]/math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Ry(-90,'deg').vec, np.r_[1,0,-1,0]/math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rz(90,'deg').vec, np.r_[1,0,0,1]/math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rz(-90,'deg').vec, np.r_[1,0,0,-1]/math.sqrt(2))
             
-            nt.assert_array_almost_equal(UnitQuaternion.Rx(90,'deg').vec, np.r_[1,1,0,0]/math.sqrt(2))
-            nt.assert_array_almost_equal(UnitQuaternion.Rx(-90,'deg').vec, np.r_[1,-1,0,0]/math.sqrt(2))
-            nt.assert_array_almost_equal(UnitQuaternion.Ry(90,'deg').vec, np.r_[1,0,1,0]/math.sqrt(2))
-            nt.assert_array_almost_equal(UnitQuaternion.Ry(-90,'deg').vec, np.r_[1,0,-1,0]/math.sqrt(2))
-            nt.assert_array_almost_equal(UnitQuaternion.Rz(90,'deg').vec, np.r_[1,0,0,1]/math.sqrt(2))
-            nt.assert_array_almost_equal(UnitQuaternion.Rz(-90,'deg').vec, np.r_[1,0,0,-1]/math.sqrt(2))
-            
+# ---------------------------------------------------------------------------------------#
+if __name__ == '__main__':
+
+    
     unittest.main()
