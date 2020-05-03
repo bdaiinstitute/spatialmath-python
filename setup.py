b1ec43fa1ec4534b121bb07f355df92c8c2fa533
Peter Corke <peter.i.corke@gmail.com>
2020-05-03 20:04:28 +1000

    added metadata

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='spatialmath-python', 
 
-    version='0.1', #TODO 
+    version='0.5', #TODO 
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -19,6 +19,28 @@ setup(
     
     long_description=long_description,
     long_description_content_type='text/markdown',
+
+    classifiers=[
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3 :: Only'],
+
+    project_urls={
+        'Documentation': 'https://petercorke.github.io/spatialmath-python',
+        'Source': 'https://github.com/petercorke/spatialmath-python',
+        'Tracker': 'https://github.com/petercorke/spatialmath-python/issues',
+        'Coverage': 'https://codecov.io/gh/petercorke/spatialmath-python'
+    },
 
     url='https://github.com/petercorke/spatialmath-python',
 
@@ -28,7 +50,7 @@ setup(
 
     keywords='python SO2 SE2 SO3 SE3 rotation euler roll-pitch-yaw quaternion transforms robotics vision pose',
 
-    license='', #TODO
+    license='MIT', #TODO
 
     python_requires='>=3.2',
 
