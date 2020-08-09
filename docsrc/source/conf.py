7c1a62947027fd7083d7a2452076c4485c9a5d43
Peter Corke <peter.i.corke@gmail.com>
2020-08-09 14:07:08 +1000

    change to RTD doc theme

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -65,8 +65,8 @@ exclude_patterns = ['test_*']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
 #html_theme = 'pyramid'
 #html_theme = 'sphinxdoc'
 
