big push on quaternion class doco minor fixes along the way, consistency with pose3d

spatialmath/base/quaternions.py
@@ -1,35 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 10 14:12:56 2020
 
-@author: Peter Corke
-"""
-
-# This file is part of the SpatialMath toolbox for Python
-# https://github.com/petercorke/spatialmath-python
-# 
-# MIT License
-# 
-# Copyright (c) 1993-2020 Peter Corke
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 # Contributors:
 # 
@@ -155,7 +125,7 @@ def isequal(q1, q2, tol=100, unitq=False):
     q1 = argcheck.getvector(q1, 4)
     q2 = argcheck.getvector(q2, 4)
 
-    if unit:
+    if unitq:
         return (np.sum(np.abs(q1 - q2)) < tol * _eps) or (np.sum(np.abs(q1 + q2)) < tol * _eps)
     else:
         return (np.sum(np.abs(q1 - q2)) < tol * _eps)
@@ -574,7 +544,7 @@ def dotb(q, w):
     :return: rate of change of unit quaternion
     :rtype: numpy.ndarray, shape=(4,)
 
-    ``dot(q, w)`` is the rate of change of the elements of the unit quaternion ``q``
+    ``dotb(q, w)`` is the rate of change of the elements of the unit quaternion ``q``
     which represents the orientation of a body frame with angular velocity ``w`` in
     the body frame.
 

spatialmath/pose3d.py
@@ -8,6 +8,23 @@ from spatialmath.base import argcheck
 from spatialmath import base as tr
 from spatialmath import super_pose as sp
 
+"""
+Classes to abstract 3D pose and orientation using matrices in SO(3) and SE(3)
+
+To use::
+
+    from spatialmath.pose3d import *
+    T = SE3.Rx(0.3)
+
+    import spatialmath as sm
+    T = sm.SE3.Rx(0.3)
+
+
+ .. inheritance-diagram:: spatialmath.pose3d
+    :top-classes: collections.UserList
+    :parts: 1
+"""
+
 # ============================== SO3 =====================================#
 
 
@@ -18,7 +35,9 @@ class SO3(sp.SMPose):
     This subclass represents rotations in 3D space.  Internally it is a 3x3 orthogonal matrix belonging
     to the group SO(3).
 
-    .. inheritance-diagram::
+ .. inheritance-diagram:: spatialmath.pose3d.SO3
+    :top-classes: collections.UserList
+    :parts: 1
     """
 
     def __init__(self, arg=None, *, check=True):
@@ -185,6 +204,36 @@ class SO3(sp.SMPose):
             return tr.tr2rpy(self.A, unit=unit, order=order)
         else:
             return np.array([tr.tr2rpy(x, unit=unit, order=order) for x in self.A]).T
+
+    def angvec(self, unit='rad'):
+        r"""
+        SO(3) or SE(3) as angle and rotation vector
+
+        :param unit: angular units: 'rad' [default], or 'deg'
+        :type unit: str
+        :param check: check that rotation matrix is valid
+        :type check: bool
+        :return: :math:`(\theta, {\bf v})`
+        :rtype: float, numpy.ndarray, shape=(3,)
+
+        ``q.angvec()`` is a tuple :math:`(\theta, v)` containing the rotation 
+        angle and a rotation axis which is equivalent to the rotation of
+        the unit quaternion ``q``.
+
+        By default the angle is in radians but can be changed setting `unit='deg'`.
+
+        Notes:
+
+        - If the input is SE(3) the translation component is ignored.
+
+        Example::
+
+        >>> UnitQuaternion.Rz(0.3).angvec()
+            (0.3, array([0., 0., 1.]))
+
+        :seealso: :func:`~spatialmath.quaternion.AngVec`, :func:`~angvec2r`
+        """
+        return tr.tr2angvec(self.R, unit=unit)
 
     def Ad(self):
         """
@@ -486,6 +535,16 @@ class SO3(sp.SMPose):
 
 
 class SE3(SO3):
+    """
+    SE(3) subclass
+
+    This subclass represents rigid-body motion in 3D space.  Internally it is a 4x4 homogeneous transformation matrix belonging
+    to the group SE(3).
+
+ .. inheritance-diagram:: spatialmath.pose3d.SE3
+    :top-classes: collections.UserList
+    :parts: 1
+    """
 
     def __init__(self, x=None, y=None, z=None, *, check=True):
         """
@@ -914,6 +973,5 @@ class SE3(SO3):
 if __name__ == '__main__':   # pragma: no cover
 
     import pathlib
-    import os.path
 
     exec(open(os.path.join(pathlib.Path(__file__).parent.absolute(), "test_pose3d.py")).read())

spatialmath/test_quaternion.py
@@ -23,12 +23,12 @@ class TestUnitQuaternion(unittest.TestCase):
     def test_constructor(self):
         nt.assert_array_almost_equal(UnitQuaternion().vec, np.r_[1, 0, 0, 0])
 
-        nt.assert_array_almost_equal(UnitQuaternion.Rx(90, 'deg').vec, np.r_[1, 1, 0, 0] / math.math.sqrt(2))
-        nt.assert_array_almost_equal(UnitQuaternion.Rx(-90, 'deg').vec, np.r_[1, -1, 0, 0] / math.math.sqrt(2))
-        nt.assert_array_almost_equal(UnitQuaternion.Ry(90, 'deg').vec, np.r_[1, 0, 1, 0] / math.math.sqrt(2))
-        nt.assert_array_almost_equal(UnitQuaternion.Ry(-90, 'deg').vec, np.r_[1, 0, -1, 0] / math.math.sqrt(2))
-        nt.assert_array_almost_equal(UnitQuaternion.Rz(90, 'deg').vec, np.r_[1, 0, 0, 1] / math.math.sqrt(2))
-        nt.assert_array_almost_equal(UnitQuaternion.Rz(-90, 'deg').vec, np.r_[1, 0, 0, -1] / math.math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rx(90, 'deg').vec, np.r_[1, 1, 0, 0] / math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rx(-90, 'deg').vec, np.r_[1, -1, 0, 0] / math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Ry(90, 'deg').vec, np.r_[1, 0, 1, 0] / math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Ry(-90, 'deg').vec, np.r_[1, 0, -1, 0] / math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rz(90, 'deg').vec, np.r_[1, 0, 0, 1] / math.sqrt(2))
+        nt.assert_array_almost_equal(UnitQuaternion.Rz(-90, 'deg').vec, np.r_[1, 0, 0, -1] / math.sqrt(2))
 
     def test_constructor(self):
 
@@ -120,14 +120,13 @@ class TestUnitQuaternion(unittest.TestCase):
         self.assertIsInstance(uu, UnitQuaternion)
         self.assertEqual(len(uu), 4)
 
-    def primitive_test_convert(self):
+    def test_convert_string(self):
         # char
 
         u = UnitQuaternion()
 
-        s = char(u)
-        tc.verifyClass(s, 'char')
-        s = char([u, u, u])
+        s = str(u)
+        self.assertIsInstance(s, str)
 
         # s,v
         nt.assert_array_almost_equal(UnitQuaternion([1, 0, 0, 0]).s, 1)
@@ -143,7 +142,7 @@ class TestUnitQuaternion(unittest.TestCase):
         nt.assert_array_almost_equal(UnitQuaternion([0, 0, 0, 1]).v, [0, 0, 1])
 
         # R,T
-        nt.assert_array_almost_equal(u.R, eye(3, 3))
+        nt.assert_array_almost_equal(u.R, eye(3))
 
         nt.assert_array_almost_equal(UnitQuaternion(rotx(pi / 2)).R, rotx(pi / 2))
         nt.assert_array_almost_equal(UnitQuaternion(roty(-pi / 2)).R, roty(-pi / 2))
@@ -195,8 +194,8 @@ class TestUnitQuaternion(unittest.TestCase):
         # (theta, v)
         th = 0.2
         v = unitvec([1, 2, 3])
-        nt.assert_array_almost_equal(UnitQuaternion.Omega(th * v).R, angvec2r(th, v))
-        nt.assert_array_almost_equal(UnitQuaternion.Omega(-th * v).R, angvec2r(-th, v))
+        nt.assert_array_almost_equal(UnitQuaternion.EulerVec(th * v).R, angvec2r(th, v))
+        nt.assert_array_almost_equal(UnitQuaternion.EulerVec(-th * v).R, angvec2r(-th, v))
 
     def test_canonic(self):
         R = rotx(0)
@@ -278,12 +277,12 @@ class TestUnitQuaternion(unittest.TestCase):
 
         self.assertIsInstance(u / u, UnitQuaternion)
 
-        self.assertIsInstance(u.conj, UnitQuaternion)
-        self.assertIsInstance(u.inv, UnitQuaternion)
-        self.assertIsInstance(u.unit, UnitQuaternion)
-        self.assertIsInstance(q.unit, UnitQuaternion)
+        self.assertIsInstance(u.conj(), UnitQuaternion)
+        self.assertIsInstance(u.inv(), UnitQuaternion)
+        self.assertIsInstance(u.unit(), UnitQuaternion)
+        self.assertIsInstance(q.unit(), UnitQuaternion)
 
-        self.assertIsInstance(q.conj, Quaternion)
+        self.assertIsInstance(q.conj(), Quaternion)
 
         self.assertIsInstance(q + q, Quaternion)
         self.assertIsInstance(q - q, Quaternion)
@@ -298,8 +297,8 @@ class TestUnitQuaternion(unittest.TestCase):
         # self.assertIsInstance(u-q, Quaternion)
         # TODO test for ValueError in these cases
 
-        self.assertIsInstance(u.SO3, SO3)
-        self.assertIsInstance(u.SE3, SE3)
+        self.assertIsInstance(u.SO3(), SO3)
+        self.assertIsInstance(u.SE3(), SE3)
 
     def test_multiply(self):
 
@@ -394,9 +393,9 @@ class TestUnitQuaternion(unittest.TestCase):
     def test_conversions(self):
 
         # , 3 angle
-        qcompare(UnitQuaternion.RPY([0.1, 0.2, 0.3]).rpy, [0.1, 0.2, 0.3])
+        qcompare(UnitQuaternion.RPY([0.1, 0.2, 0.3]).rpy(), [0.1, 0.2, 0.3])
 
-        qcompare(UnitQuaternion.Eul([0.1, 0.2, 0.3]).eul, [0.1, 0.2, 0.3])
+        qcompare(UnitQuaternion.Eul([0.1, 0.2, 0.3]).eul(), [0.1, 0.2, 0.3])
 
         qcompare(UnitQuaternion.RPY([10, 20, 30], unit='deg').R, rpy2r(10, 20, 30, unit='deg'))
 
@@ -405,18 +404,18 @@ class TestUnitQuaternion(unittest.TestCase):
         # (theta, v)
         th = 0.2
         v = unitvec([1, 2, 3])
-        [a, b] = UnitQuaternion.AngVec(th, v).angvec
+        [a, b] = UnitQuaternion.AngVec(th, v).angvec()
         self.assertAlmostEqual(a, th)
         nt.assert_array_almost_equal(b, v)
 
-        [a, b] = UnitQuaternion.AngVec(-th, v).angvec
+        [a, b] = UnitQuaternion.AngVec(-th, v).angvec()
         self.assertAlmostEqual(a, th)
         nt.assert_array_almost_equal(b, -v)
 
         # null rotation case
         th = 0
         v = unitvec([1, 2, 3])
-        [a, b] = UnitQuaternion.AngVec(th, v).angvec
+        [a, b] = UnitQuaternion.AngVec(th, v).angvec()
         self.assertAlmostEqual(a, th)
 
     #  SO3                     convert to SO3 class
@@ -432,12 +431,12 @@ class TestUnitQuaternion(unittest.TestCase):
         u = UnitQuaternion()
 
         # norm
-        qcompare(rx.norm, 1)
-        qcompare(UnitQuaternion([rx, ry, rz]).norm, [1, 1, 1])
+        qcompare(rx.norm(), 1)
+        qcompare(UnitQuaternion([rx, ry, rz]).norm(), [1, 1, 1])
 
         # unit
-        qcompare(rx.unit, rx)
-        qcompare(UnitQuaternion([rx, ry, rz]).unit, UnitQuaternion([rx, ry, rz]))
+        qcompare(rx.unit(), rx)
+        qcompare(UnitQuaternion([rx, ry, rz]).unit(), UnitQuaternion([rx, ry, rz]))
 
         # inner
         nt.assert_array_almost_equal(u.inner(u), 1)
@@ -447,7 +446,7 @@ class TestUnitQuaternion(unittest.TestCase):
         q = rx * ry * rz
 
         qcompare(q**0, u)
-        qcompare(q**(-1), q.inv)
+        qcompare(q**(-1), q.inv())
         qcompare(q**2, q * q)
 
         # angle
@@ -531,8 +530,8 @@ class TestUnitQuaternion(unittest.TestCase):
         nt.assert_array_almost_equal(q.dotb(omega), np.r_[0, omega / 2])
 
         q = UnitQuaternion.Rx(pi / 2)
-        qcompare(q.dot(omega), 0.5 * Quaternion.pure(omega) * q)
-        qcompare(q.dotb(omega), 0.5 * q * Quaternion.pure(omega))
+        qcompare(q.dot(omega), 0.5 * Quaternion.Pure(omega) * q)
+        qcompare(q.dotb(omega), 0.5 * q * Quaternion.Pure(omega))
 
     def test_matrix(self):
 
@@ -597,7 +596,7 @@ class TestQuaternion(unittest.TestCase):
 
         # pure
         v = [5, 6, 7]
-        nt.assert_array_almost_equal(Quaternion.pure(v).vec, [0, ] + v)
+        nt.assert_array_almost_equal(Quaternion.Pure(v).vec, [0, ] + v)
 
         # tc.verifyError( @() Quaternion.pure([1, 2]), 'SMTB:Quaternion:badarg')
 
@@ -682,8 +681,8 @@ class TestQuaternion(unittest.TestCase):
 
         # other combos all fail, test this?
 
-        self.assertIsInstance(q.conj, Quaternion)
-        self.assertIsInstance(q.unit, UnitQuaternion)
+        self.assertIsInstance(q.conj(), Quaternion)
+        self.assertIsInstance(q.unit(), UnitQuaternion)
 
         self.assertIsInstance(q + q, Quaternion)
         self.assertIsInstance(q + q, Quaternion)
@@ -824,19 +823,19 @@ class TestQuaternion(unittest.TestCase):
         u = Quaternion([1, 0, 0, 0])
 
         # norm
-        nt.assert_array_almost_equal(q.norm, np.linalg.norm(v))
-        nt.assert_array_almost_equal(Quaternion([q, u, q]).norm, [np.linalg.norm(v), 1, np.linalg.norm(v)])
+        nt.assert_array_almost_equal(q.norm(), np.linalg.norm(v))
+        nt.assert_array_almost_equal(Quaternion([q, u, q]).norm(), [np.linalg.norm(v), 1, np.linalg.norm(v)])
 
         # unit
-        qu = q.unit
+        qu = q.unit()
         u = UnitQuaternion()
         self.assertIsInstance(q, Quaternion)
         nt.assert_array_almost_equal(qu.vec, v / np.linalg.norm(v))
-        qcompare(Quaternion([q, u, q]).unit, UnitQuaternion([qu, u, qu]))
+        qcompare(Quaternion([q, u, q]).unit(), UnitQuaternion([qu, u, qu]))
 
         # inner
         nt.assert_equal(u.inner(u), 1)
-        nt.assert_equal(q.inner(q), q.norm**2)
+        nt.assert_equal(q.inner(q), q.norm()**2)
         nt.assert_equal(q.inner(u), np.dot(q.vec, u.vec))
 
 