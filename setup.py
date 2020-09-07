1068d97daa86f9718b69016dda970257dd7a211d
Peter Corke <peter.i.corke@gmail.com>
2020-09-07 18:36:58 +1000

    minor formatting, bump release to 0.8

--- setup.py
+++ setup.py
@@ -13,31 +13,31 @@ with open(path.join(here, 'RELEASE'), encoding='utf-8') as f:
 
 
 setup(
-    name='spatialmath-python', 
+    name='spatialmath-python',
 
     version=release,
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
-    description='Provides spatial maths capability for Python.', #TODO
-    
+    description='Provides spatial maths capability for Python.',  # TODO
+
     long_description=long_description,
     long_description_content_type='text/markdown',
 
     classifiers=[
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-    # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: MIT License',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 3 :: Only'],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only'],
 
     project_urls={
         'Documentation': 'https://petercorke.github.io/spatialmath-python',
@@ -50,16 +50,16 @@ setup(
 
     author='Peter Corke',
 
-    author_email='rvc@petercorke.com', #TODO
+    author_email='rvc@petercorke.com',  # TODO
 
     keywords='python SO2 SE2 SO3 SE3 rotation euler roll-pitch-yaw quaternion transforms robotics vision pose',
 
-    license='MIT', #TODO
+    license='MIT',  # TODO
 
     python_requires='>=3.5',
 
     packages=find_packages(exclude=["test_*", "TODO*"]),
 
     install_requires=['numpy', 'scipy', 'matplotlib']
-    
+
 )
