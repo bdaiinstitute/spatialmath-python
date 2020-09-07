ac44ac376fadcf98ef64dd22ea06d124d4a05d29
Peter Corke <peter.i.corke@gmail.com>
2020-09-07 11:35:49 +1000

    import just the types, no superclasses

--- spatialmath/__init__.py
+++ spatialmath/__init__.py
@@ -1,9 +1,8 @@
-from spatialmath.pose2d import *
-from spatialmath.pose3d import *
-from spatialmath.geom3d import *
-from spatialmath.twist import *
-from spatialmath.spatialvector import *
-from spatialmath.quaternion import *
-from spatialmath.super_pose import *
-from spatialmath import base
+from spatialmath.pose2d import SO2, SE2
+from spatialmath.pose3d import SO3, SE3
+from spatialmath.geom3d import Plucker, Plane
+from spatialmath.twist import Twist3, Twist2
+from spatialmath.spatialvector import SpatialVelocity, SpatialAcceleration, \
+     SpatialForce, SpatialMomentum, SpatialInertia
+from spatialmath.quaternion import Quaternion, UnitQuaternion
 #from spatialmath.Plucker import *
