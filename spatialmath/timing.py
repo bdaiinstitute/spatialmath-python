7bb78d01b65308ffd40c0f6a129ac35db9017dc4
Peter Corke <peter.i.corke@gmail.com>
2020-09-23 07:31:09 +1000

    update for delving into performance issues

--- spatialmath/timing.py
+++ spatialmath/timing.py
@@ -11,31 +11,101 @@ if __name__ == '__main__':
 
     import timeit
 
-    N = 1000000
+    N = 100000
 
-    quat_setup = '''import spatialmath.base as tr
+
+
+    transforms_setup = '''
+import spatialmath as sm
+import numpy as np
+from collections import namedtuple
+Rt = namedtuple('Rt', 'R t')
+X1 = sm.SE3.Rand()
+X2 = sm.SE3.Rand()
+T1 = X1.A
+T2 = X2.A
+R1 = sm.base.t2r(T1)
+R2 = sm.base.t2r(T2)
+t1 = sm.base.transl(T1)
+t2 = sm.base.transl(T2)
+Rt1 = Rt(R1, t1)
+Rt2 = Rt(R2, t2)
+'''
+    t = timeit.timeit(stmt='sm.base.getvector(0.2)', setup=transforms_setup, number=N)
+    print(f"getvector(x):        {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.base.rotx(0.2, unit="rad")', setup=transforms_setup, number=N)
+    print(f"transforms.rotx:     {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.base.trotx(0.2, unit="rad")', setup=transforms_setup, number=N)
+    print(f"transforms.trotx:    {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.base.t2r(T1)', setup=transforms_setup, number=N)
+    print(f"transforms.t2r:      {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.base.r2t(R1)', setup=transforms_setup, number=N)
+    print(f"transforms.r2t:      {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.SE3.Rx(0.2)', setup=transforms_setup, number=N)
+    print(f"SE3.Rx:              {t:.3g} μs")
+
+    t = timeit.timeit(stmt='T1[:3,:3]', setup=transforms_setup, number=N)
+    print(f"T1[:3,:3]:           {t:.3g} μs")
+
+    t = timeit.timeit(stmt='X1.A', setup=transforms_setup, number=N)
+    print(f"SE3.A:               {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.SE3()', setup=transforms_setup, number=N)
+    print(f"SE3():               {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.SE3(T1)', setup=transforms_setup, number=N)
+    print(f"SE3(T1):             {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.SE3(T1, check=False)', setup=transforms_setup, number=N)
+    print(f"SE3(T1 check=False): {t:.3g} μs")
+
+    t = timeit.timeit(stmt='X1 * X2', setup=transforms_setup, number=N)
+    print(f"SE3 *:               {t:.3g} μs")
+
+    t = timeit.timeit(stmt='T1 @ T2', setup=transforms_setup, number=N)
+    print(f"4x4 @:               {t:.3g} μs")
+
+    t = timeit.timeit(stmt='T1[:3,:3] @ T2[:3,:3] + T1[:3,:3] @ T2[:3,3]', setup=transforms_setup, number=N)
+    print(f"R1*R2, R1*t:         {t:.3g} μs")
+
+    t = timeit.timeit(stmt='(Rt1.R @ Rt2.R, Rt1.R @ Rt2.t)', setup=transforms_setup, number=N)
+    print(f"T1 * T2 (R, t):      {t:.3g} μs")
+
+    t = timeit.timeit(stmt='X1.inv()', setup=transforms_setup, number=N)
+    print(f"SE3.inv:             {t:.3g} μs")
+
+    t = timeit.timeit(stmt='sm.base.trinv(T1)', setup=transforms_setup, number=N)
+    print(f"base.trinv:          {t:.3g} μs")
+
+    t = timeit.timeit(stmt='(Rt1.R.T, -Rt1.R.T @ Rt1.t)', setup=transforms_setup, number=N)
+    print(f"base.trinv (R,t):    {t:.3g} μs")
+
+    t = timeit.timeit(stmt='np.linalg.inv(T1)', setup=transforms_setup, number=N)
+    print(f"np.linalg.inv:       {t:.3g} μs")
+
+if False:
+    quat_setup = '''
+import spatialmath.base as tr
 import spatialmath.quaternion as qq
 import numpy as np
 q1 = tr.rand()
 q2 = tr.rand()
 v = np.r_[1,2,3]
 Q1 = qq.UnitQuaternion.Rx(0.2)
-Q2 = qq.UnitQuaternion.Ry(0.3)'''
-
-    transforms_setup = '''import spatialmath.base as tr'''
-
-
-    t = timeit.timeit(stmt='tr.rotx(0.2)', setup=transforms_setup, number=N)
-    print('transforms.rotx: ', t, ' us')
-
-
+Q2 = qq.UnitQuaternion.Ry(0.3)
+'''
     t = timeit.timeit(stmt='a = tr.qqmul(q1,q2)', setup=quat_setup, number=N)
-    print('quat.qqmul:         ', t, ' us')
+    print(f"quat.qqmul:         {t:.3g} μs")
     t = timeit.timeit(stmt='a = tr.qvmul(q1,v)', setup=quat_setup, number=N)
-    print('quat.qqmul:         ', t, ' us')
+    print(f"quat.qqmul:         {t:.3g} μs")
     t = timeit.timeit(stmt='a = qq.UnitQuaternion()', setup=quat_setup, number=N)
-    print('UnitQuaternion() :  ', t, ' us')
+    print(f"UnitQuaternion() :  {t:.3g} μs")
     t = timeit.timeit(stmt='a = qq.UnitQuaternion.Rx(0.2)', setup=quat_setup, number=N)
-    print('UnitQuaternion.Rx : ', t, ' us')
+    print(f"UnitQuaternion.Rx : {t:.3g} μs")
     t = timeit.timeit(stmt='a = Q1 * Q2', setup=quat_setup, number=N)
-    print('UnitQuaternion *:   ', t, ' us')
+    print(f"UnitQuaternion *:   {t:.3g} μs")
