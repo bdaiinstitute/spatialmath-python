4c546bb67bb5d6da24ed5fcfc5d6b1dba5eaabd5
Peter Corke <peter.i.corke@gmail.com>
2020-11-02 08:17:38 +1000

    add autorun support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,3 +111,12 @@ latex_elements = {
 
 autorun_languages = {}
 autorun_languages['pycon_output_encoding'] = 'UTF-8'
+autorun_languages['pycon_input_encoding'] = 'UTF-8'
+autorun_languages['pycon_runfirst'] = """
+from spatialmath import SE3
+SE3._color = False
+import numpy as np
+np.set_printoptions(precision=4, suppress=True)
+from ansitable import ANSITable
+ANSITable._color = False
+"""
