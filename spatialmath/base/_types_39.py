524f0392bdc1784ec980af32d896edf0be8e418d
Peter Corke <peter.i.corke@gmail.com>
2023-02-20 09:26:21 +1000

    tidy up type definitions

--- spatialmath/base/_types_39.py
+++ spatialmath/base/_types_39.py
@@ -20,8 +20,6 @@ import numpy as np
 from numpy import ndarray, dtype, floating
 from numpy.typing import NDArray, DTypeLike
 
-print("*************** _types_39 *************")
-
 # array like
 
 # these are input to many functions in spatialmath.base, and can be a list, tuple or
@@ -72,43 +70,42 @@ ArrayLike6 = Union[
 ]
 
 # real vectors
+R1 = ndarray[
+    Tuple[L[1]],
+    dtype[floating],
+]  # R^1
 R2 = ndarray[
-    Tuple[L[2,]],
+    Tuple[L[2]],
     dtype[floating],
 ]  # R^2
 R3 = ndarray[
-    Tuple[L[3,]],
+    Tuple[L[3]],
     dtype[floating],
 ]  # R^3
 R4 = ndarray[
-    Tuple[L[4,]],
+    Tuple[L[4]],
     dtype[floating],
-]  # R^3
-# R6 = ndarray[
-#     Tuple[
-#         L[
-#             6,
-#         ]
-#     ],
-#     dtype[floating],
-# ]  # R^6
-R6 = NDArray[floating]
+]  # R^4
+R6 = ndarray[
+    Tuple[L[6]],
+    dtype[floating],
+]  # R^6
 R8 = ndarray[
-    Tuple[L[8,]],
+    Tuple[L[8]],
     dtype[floating],
 ]  # R^8
 
 # real matrices
-R1x1 = ndarray[Tuple[L[1, 1]], dtype[floating]]  # R^{1x1} matrix
-R2x2 = ndarray[Tuple[L[2, 2]], dtype[floating]]  # R^{2x2} matrix
-R3x3 = ndarray[Tuple[L[3, 3]], dtype[floating]]  # R^{3x3} matrix
-R4x4 = ndarray[Tuple[L[4, 4]], dtype[floating]]  # R^{4x4} matrix
-R6x6 = ndarray[Tuple[L[6, 6]], dtype[floating]]  # R^{6x6} matrix
-R8x8 = ndarray[Tuple[L[8, 8]], dtype[floating]]  # R^{8x8} matrix
-R1x3 = ndarray[Tuple[L[1, 3]], dtype[floating]]  # R^{1x3} row vector
-R3x1 = ndarray[Tuple[L[3, 1]], dtype[floating]]  # R^{3x1} column vector
-R1x2 = ndarray[Tuple[L[1, 2]], dtype[floating]]  # R^{1x2} row vector
-R2x1 = ndarray[Tuple[L[2, 1]], dtype[floating]]  # R^{2x1} column vector
+R1x1 = ndarray[Tuple[L[1], L[1]], dtype[floating]]  # R^{1x1} matrix
+R2x2 = ndarray[Tuple[L[2], L[2]], dtype[floating]]  # R^{2x2} matrix
+R3x3 = ndarray[Tuple[L[3], L[3]], dtype[floating]]  # R^{3x3} matrix
+R4x4 = ndarray[Tuple[L[4], L[4]], dtype[floating]]  # R^{4x4} matrix
+R6x6 = ndarray[Tuple[L[6], L[6]], dtype[floating]]  # R^{6x6} matrix
+R8x8 = ndarray[Tuple[L[8], L[8]], dtype[floating]]  # R^{8x8} matrix
+R1x3 = ndarray[Tuple[L[1], L[3]], dtype[floating]]  # R^{1x3} row vector
+R3x1 = ndarray[Tuple[L[3], L[1]], dtype[floating]]  # R^{3x1} column vector
+R1x2 = ndarray[Tuple[L[1], L[2]], dtype[floating]]  # R^{1x2} row vector
+R2x1 = ndarray[Tuple[L[2], L[1]], dtype[floating]]  # R^{2x1} column vector
 
 # Points2 = ndarray[Tuple[L[2, Any]], dtype[floating]]  # R^{2xN} matrix
 # Points3 = ndarray[Tuple[L[3, Any]], dtype[floating]]  # R^{2xN} matrix
@@ -121,7 +118,7 @@ RNx3 = NDArray
 # Lie group elements
 SO2Array = ndarray[Tuple[L[2, 2]], dtype[floating]]  # SO(2) rotation matrix
 SE2Array = ndarray[Tuple[L[3, 3]], dtype[floating]]  # SE(2) rigid-body transform
-# SO3Array = ndarray[Tuple[L[3, 3]], dtype[floating]]  
+# SO3Array = ndarray[Tuple[L[3, 3]], dtype[floating]]
 SO3Array = np.ndarray[Tuple[L[3], L[3]], dtype[floating]]  # SO(3) rotation matrix
 SE3Array = ndarray[Tuple[L[4], L[4]], dtype[floating]]  # SE(3) rigid-body transform
 
@@ -159,5 +156,3 @@ sonArray = Union[so2Array, so3Array]
 senArray = Union[se2Array, se3Array]
 
 Color = Union[str, ArrayLike3]
-
-print(SO3Array)
--- spatialmath/base/types.py
+++ spatialmath/base/types.py
@@ -2,13 +2,10 @@ import sys
 
 _version = sys.version_info.minor
 
-# from spatialmath.base._types_39 import *
 
 if _version >= 11:
     from spatialmath.base._types_311 import *
 elif _version >= 9:
-    from spatialmath.base._types_311 import *
+    from spatialmath.base._types_39 import *
 else:
-    from spatialmath.base._types_311 import *
-
-# pass
+    from spatialmath.base._types_35 import *
