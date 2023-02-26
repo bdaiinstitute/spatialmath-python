af45b97944471d5f468f326ea01a42a42b6aee0a
Andre ASTYL <andre.astyl@ariane.group>
2023-02-26 13:46:30 +0100

    At 3d plot: apply color, textcolor or origincolor

--- spatialmath/base/transforms3d.py
+++ spatialmath/base/transforms3d.py
@@ -2933,15 +2933,6 @@ def trplot(
 
     # label the frame
     if frame:
-        if textcolor is None:
-            textcolor = color[0]
-        else:
-            textcolor = "blue"
-        if origincolor is None:
-            origincolor = color[0]
-        else:
-            origincolor = "black"
-
         o1 = T @ np.array(np.r_[flo, 1])
         ax.text(
             o1[0],
