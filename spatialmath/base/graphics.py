c8141fb89939815cf454ad7ad22539f2a39879d4
Peter Corke <peter.i.corke@gmail.com>
2023-03-12 09:23:25 +1000

    embellish examples for plot_arrow

--- spatialmath/base/graphics.py
+++ spatialmath/base/graphics.py
@@ -507,7 +507,7 @@ try:
         ax: Optional[plt.Axes] = None,
         **kwargs,
     ) -> List[plt.Artist]:
-        """
+        r"""
         Plot 2D arrow
 
         :param start: start point, arrow tail
@@ -535,20 +535,22 @@ try:
 
             >>> from spatialmath.base import plotvol2, plot_arrow
             >>> plotvol2(5)
-            >>> plot_arrow((-2, 2), (3, 4), color='r', width=0.1)  # red arrow
+            >>> plot_arrow((-2, 2), (2, 4), color='r', width=0.1)  # red arrow
+            >>> plot_arrow((4, 1), (2, 4), color='b', width=0.1)  # blue arrow
 
         .. plot::
 
             from spatialmath.base import plotvol2, plot_arrow
             ax = plotvol2(5)
             plot_arrow((-2, 2), (3, 4), color='r', width=0.1)  # red arrow
+            plot_arrow((4, 1), (3, 4), color='b', width=0.1)  # blue arrow
             ax.grid()
 
         Example::
 
             >>> from spatialmath.base import plotvol2, plot_arrow
             >>> plotvol2(5)
-            >>> plot_arrow((-2, -2), (2, 4), label="$\mathit{p}_3$", color='r', width=0.1)
+            >>> plot_arrow((-2, -2), (2, 4), label=r"$\mathit{p}_3$", color='r', width=0.1)
 
         .. plot::
 
@@ -556,7 +558,7 @@ try:
             ax = plotvol2(5)
             ax.grid()
             plot_arrow(
-                (-2, 2), (3, 4), label="$\mathit{p}_3$", color="r", width=0.1
+                (-2, -2), (2, 4), label="$\mathit{p}_3$", color="r", width=0.1
             )
             plt.show(block=True)
 
