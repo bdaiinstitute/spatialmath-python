5c2612e7afea2ba62960ba115c34589e8872102e
GitHub Action <action@github.com>
2023-03-11 23:43:48 +0000

    Update documentation

--- func_2d_graphics-1.py
+++ func_2d_graphics-1.py
@@ -1,4 +1,5 @@
 from spatialmath.base import plotvol2, plot_arrow
 ax = plotvol2(5)
 plot_arrow((-2, 2), (3, 4), color='r', width=0.1)  # red arrow
+plot_arrow((4, 1), (3, 4), color='b', width=0.1)  # blue arrow
 ax.grid()
