49420a6a3222fc6f12a0ec742562ef310c7a9682
Mark Yeatman <myeatman@theaiinstitute.com>
2024-07-18 13:04:46 -0400

    Working fit example.

--- spatialmath/spline.py
+++ spatialmath/spline.py
@@ -80,16 +80,22 @@ class CubicBSplineSE3:
 
         t: Normalized time value [0,1] to evaluate the spline at.
         """     
-        current_pose = SE3()
+        
         
         spline_no_coeff = BSpline.design_matrix([time], self.knots, self.degree)  #the B in sum(alpha_i * B_i) = S(t)
-
         rows,cols = spline_no_coeff.nonzero()
-        for row,col in zip(rows,cols): 
-            control_pose: SE3 = self.control_poses[col]
-            current_pose = control_pose.interp1(spline_no_coeff[row,col]) * current_pose
 
-        return current_pose
+        current_so3 = SO3()
+        for row,col in zip(rows,cols): 
+            control_so3: SO3 = SO3(self.control_poses[col])
+            current_so3 = control_so3.interp1(spline_no_coeff[row,col]) * current_so3
+
+        xyz = np.array([0,0,0])
+        for row,col in zip(rows,cols): 
+            control_point = self.control_poses[col].t
+            xyz = xyz + control_point*spline_no_coeff[row,col]
+
+        return SE3.Rt(t = xyz, R = current_so3)
 
     @classmethod
     def knots_from_num_control_poses(self, num_control_poses: int):
@@ -135,14 +141,14 @@ class CubicBSplineSE3:
         
 class FitCubicBSplineSE3:
 
-    def __init__(self, pose_data: List[SE3], timestamps) -> None:
+    def __init__(self, pose_data: List[SE3], timestamps, num_control_points: int =6) -> None:
         """ 
             Timestamps should be normalized to [0,1] and sorted.
             Outputs control poses, but the optimization is done on the [pos,axis-angle], flattened into a 1d vector.
         """
         self.pose_data = pose_data
         self.timestamps = timestamps
-        self.num_control_pose = 4
+        self.num_control_pose = num_control_points
         self.se3_rep_size = 6 #[x, y, z, axis-angle vec]
 
         # 
@@ -155,7 +161,7 @@ class FitCubicBSplineSE3:
                 closest_timestamp_indices[i] = len(timestamps) - 1
         
         # 
-        self.spline = CubicBSplineSE3(control_poses=[self.pose_data[index] for index in closest_timestamp_indices])
+        self.spline = CubicBSplineSE3(control_poses=[SE3.CopyFrom(self.pose_data[index]) for index in closest_timestamp_indices])
 
     @classmethod
     def make_SE3_pose(self, row: np.ndarray):
@@ -169,8 +175,7 @@ class FitCubicBSplineSE3:
         so3_twist = SO3(pose.R).log(twist=True)
         return np.concatenate([pose.t, so3_twist])
 
-
-    def objective_function_pos(self, pos: np.ndarray):
+    def objective_function_xyz(self, pos: np.ndarray):
         "L1 norm of SE3 distances between data points and spline"
 
         # data massage
@@ -180,8 +185,7 @@ class FitCubicBSplineSE3:
         # objective
         error_vector = [ weighted_average_SE3_metric(spline(t), pose, w = 0.0) for t,pose in zip(self.timestamps, self.pose_data, strict=True) ]
         
-        return np.linalg.norm(error_vector, ord=2)
-
+        return np.linalg.norm(error_vector, ord=np.inf)
 
     def objective_function_so3(self, so3_twists_flat: np.ndarray):
         "L1 norm of SE3 distances between data points and spline"
@@ -191,40 +195,58 @@ class FitCubicBSplineSE3:
         spline = CubicBSplineSE3(control_poses=[SE3(SO3.Exp(row)) for row in so3_twists_matrix])
 
         # objective
-        error_vector = [ weighted_average_SE3_metric(spline(t), pose, w = 0.0) for t,pose in zip(self.timestamps, self.pose_data, strict=True) ]
+        error_vector = [ weighted_average_SE3_metric(spline(t), pose, w = 1.0) for t,pose in zip(self.timestamps, self.pose_data, strict=True) ]
         
-        return np.linalg.norm(error_vector, ord=2)
+        return np.linalg.norm(error_vector, ord=np.inf)
     
+    def objective_function_pose(self, control_pose_rep: np.ndarray):
+        "L1 norm of SE3 distances between data points and spline"
+
+        # data massage
+        control_pose_matrix = control_pose_rep.reshape(self.num_control_pose, self.se3_rep_size)
+        spline = CubicBSplineSE3(control_poses=[self.make_SE3_pose(row) for row in control_pose_matrix])
+
+        # objective
+        error_vector = [ weighted_average_SE3_metric(spline(t), pose) for t,pose in zip(self.timestamps, self.pose_data, strict=True) ]
+        
+        return np.linalg.norm(error_vector, ord=np.inf)
+
     def fit(self, disp: bool = False):
 
-        pos_result = self.fit_pos(disp)
         so3_result = self.fit_so3(disp)
-
-        control_pose_matrix = np.hstack([
-            pos_result.x.reshape(self.num_control_pose, 3),
-            so3_result.x.reshape(self.num_control_pose, 3)
-        ])
+        pos_result = self.fit_xyz(disp)
         
-        self.spline = CubicBSplineSE3(control_poses=[self.make_SE3_pose(row) for row in control_pose_matrix])
         return pos_result, so3_result
 
-    def fit_pos(self, disp: bool = False):
+    def fit_xyz(self, disp: bool = False):
         
-        pos_flat = np.concatenate([pose.t for pose in self.spline.control_poses])
-        result = minimize(self.objective_function_pos, pos_flat, method="slsqp", options = {"disp":disp})    
+        xyz_flat = np.concatenate([pose.t for pose in self.spline.control_poses])
+        result = minimize(self.objective_function_xyz, xyz_flat, method="slsqp", options = {"disp":disp})   
+        xyz_mat = result.x.reshape(self.num_control_pose, 3) 
+        for i, xyz in enumerate(xyz_mat):
+            self.spline.control_poses[i].t = xyz
+
         return result
     
     def fit_so3(self, disp: bool = False):
         so3_twists_flat = np.concatenate([SO3(pose.R).log(twist=True)for pose in self.spline.control_poses])
         result = minimize(self.objective_function_so3, so3_twists_flat, method="slsqp", options = {"disp":disp})    
+        so3_twists_mat = result.x.reshape(self.num_control_pose, 3) 
+        for i, so3_twist in enumerate(so3_twists_mat):
+            self.spline.control_poses[i].R = SO3.Exp(so3_twist)
         return result
+
+    def ang_distance(self):
+        return [pose.angdist(self.spline(timestamp)) for pose, timestamp in zip(self.pose_data, self.timestamps)]
+
+    def euclidean_distance(self):
+        return [np.linalg.norm(pose.t - self.spline(timestamp).t) for pose, timestamp in zip(self.pose_data, self.timestamps)]
 
     def visualize(
         self,
         num_samples: int,
         length: float = 0.2,
         repeat: bool = False,
-        ax: Optional[plt.Axes] = None,
         kwargs_trplot: Dict[str, Any] = {"color": "green"},
         kwargs_tranimate: Dict[str, Any] = {"wait": True},
         kwargs_plot: Dict[str, Any] = {},
@@ -235,40 +257,49 @@ class FitCubicBSplineSE3:
         y = [pose.y for pose in out_poses]
         z = [pose.z for pose in out_poses]
 
-        if ax is None:
-            fig = plt.figure(figsize=(10, 10))
-            ax = fig.add_subplot(projection="3d")
+        
+        fig = plt.figure(figsize=(10, 10))
+        
+        ax = fig.add_subplot(1, 2, 1)
+        ax.plot(self.timestamps, self.ang_distance(), "--x")
+        ax.plot(self.timestamps, self.euclidean_distance(), "--x")
+        ax.legend(["angular distance", "euclidiean distance"])
+        ax.set_xlabel("Normalized time")
+
+        ax = fig.add_subplot(1, 2, 2, projection="3d")
 
         trplot(
-            [np.array(self.spline.control_poses)], ax=ax, length=length, color= "green"
-        )  # plot control points
-        trplot(
-            [np.array(self.pose_data)], ax=ax, length=length, color= "cornflowerblue", 
+            [np.array(self.spline.control_poses)], ax=ax, length=length*1.2, color= "green"
         )  # plot control points
         ax.plot(x, y, z, **kwargs_plot)  # plot x,y,z trajectory
+        trplot(
+            [np.array(self.pose_data)], ax=ax, length=length, color= "cornflowerblue", 
+        )  # plot data points
 
         tranimate(
             out_poses, repeat=repeat, length=length, **kwargs_tranimate
         )  # animate pose along trajectory
 
-def example_bspline_fit_scipy():
 
-    num_data_points = 33
+def example_bspline_fit():
+
+    num_data_points = 16
     num_samples = 100
-    frequency = 2
-    scale = 2
+    frequency = 0.5
+    scale = 4
 
     timestamps = np.linspace(0, 1, num_data_points)
     trajectory = [
-        SE3.Rt(t = [t*scale, np.sin(t * 2*np.pi* frequency), np.sin(t * 2*np.pi * frequency)], R= SO3.Rx( t*2*np.pi* frequency))
+        SE3.Rt(t = [t*scale, scale*np.sin(t * 2*np.pi* frequency), scale*np.cos(t * 2*np.pi * frequency)], 
+               R= SO3.Rx( t*2*np.pi* frequency))
         for t in timestamps
     ]
 
-    fit_se3_spline = FitCubicBSplineSE3(trajectory, timestamps)
+    fit_se3_spline = FitCubicBSplineSE3(trajectory, timestamps, num_control_points=6)
     
     result = fit_se3_spline.fit(disp=True)
-    fit_se3_spline.visualize(num_samples=num_samples, repeat=True)
+    fit_se3_spline.visualize(num_samples=num_samples, repeat=True, length=0.4, kwargs_tranimate={"wait": True, "interval" : 400})
 
 
 if __name__ == "__main__":
-    example_bspline_fit_scipy()
+    example_bspline_fit()
