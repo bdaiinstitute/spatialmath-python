1119a0bb13ae076c6c123a45375c461edf98848e
Peter Corke <peter.i.corke@gmail.com>
2025-01-19 22:29:17 +1000

    Fix the intersphinx source for matplotlib.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -187,7 +187,7 @@ np.set_printoptions(precision=4, suppress=True)
 intersphinx_mapping = {
     "numpy": ("http://docs.scipy.org/doc/numpy/", None),
     "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
-    "matplotlib": ("http://matplotlib.sourceforge.net/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
 }
 
 # -------- Options favicon -------------------------------------------------------#
